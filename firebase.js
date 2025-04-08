d8e3782c98347de79102f30b35e377cfd09e8d74
Gerald <geraldsang1550@gmail.com>
2025-04-08 22:16:27 +0300

    final-commit

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlVU4gg2hDjmxHaLmrU7BwBH0P2oXAMSg",
-  authDomain: "broadcast-ddeb4.firebaseapp.com",
+  authDomain: "https://broadcast-green.vercel.app/",
   projectId: "broadcast-ddeb4",
   storageBucket: "broadcast-ddeb4.firebasestorage.app",
   messagingSenderId: "803737868675",
